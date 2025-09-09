a3b3414521d34ed04c63e9de8246ad7e8c0d4286
MosheHM <mhm23811@gmail.com>
2025-09-09 13:23:45 +0300

    Refactor field container component: simplify template structure and remove unused email field component

--- src/app/component/field-container/field-container.component.ts
+++ src/app/component/field-container/field-container.component.ts
@@ -1,11 +1,6 @@
 import { Component, Input, Output, EventEmitter, ViewChild, ViewContainerRef, OnChanges, SimpleChanges, ComponentRef, inject, AfterViewInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FieldLogicService } from '../../services/field-logic.service';
-import { TextFieldComponent } from '../text-field/text-field.component';
-import { NumberFieldComponent } from '../number-field/number-field.component';
-import { EmailFieldComponent } from '../email-field/email-field.component';
-import { TelFieldComponent } from '../tel-field/tel-field.component';
-import { BaseFieldComponent } from '../base-field/base-field.component';
+
 
 export interface FieldConfig {
   name: string;
@@ -43,11 +38,4 @@ export class FieldContainerComponent {
  
   @Output() fieldChange = new EventEmitter<any>();
 
-  // Simple form submit handler used by template
-  onSubmit(event: Event) {
-    event.preventDefault();
-    if (!this.field) return;
-    // Emit a change event so parent can react; payload can be extended later
-    this.fieldChange.emit({ name: this.field.name });
-  }
 }
